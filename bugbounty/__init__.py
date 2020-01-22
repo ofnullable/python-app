a2444e1203b74db175ae36d224b27d452b33b32f
ofnullable <ofnullable@gmail.com>
2020-01-22 14:01:18 +0900

    :art: Change running app config

--- bugbounty/__init__.py
+++ bugbounty/__init__.py
@@ -1,36 +1,32 @@
-import os
-
 from flask import Flask
-from flask_bcrypt import Bcrypt
 from flask_restful import Api
 
-from bugbounty.database import db
-from bugbounty.env.config import config_by_env
-
-ENV = os.getenv('APP_ENV') or 'dev'
-bcrypt = Bcrypt()
+from bugbounty.settings import DevConfig
+from bugbounty.env.extensions import db, bcrypt, cors, migrate
 
 
-def create_app():
+def create_app(config=DevConfig):
     # create and configure the bugbounty
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(config_by_env[ENV])
+    app = Flask(__name__)
+    app.url_map.strict_slashes = False
+    app.config.from_object(config)
 
     db.init_app(app)
 
     api = Api(app)
 
     with app.app_context():
-
         from bugbounty.domain.program.controller import ProgramController, ProgramListController
         from bugbounty.domain.user.controller import UserController
-
-        # if ENV == 'dev':
-        db.drop_all()
-        db.create_all()
 
         api.add_resource(ProgramController, '/program', '/program/<int:id>')
         api.add_resource(ProgramListController, '/programs')
         api.add_resource(UserController, '/user')
 
         return app
+
+
+def register_extensions(app):
+    bcrypt.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
