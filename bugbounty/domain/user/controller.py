6cd09d33c0204b4b6342b479d8319fed3c9ffeb5
jooonak <ofnullable@gmail.com>
2020-01-28 19:17:17 +0900

    :construction: Implement save hacker and vendor process

--- bugbounty/domain/user/controller.py
+++ bugbounty/domain/user/controller.py
@@ -1,21 +1,40 @@
 from flask import Blueprint
 from flask_apispec import use_kwargs, marshal_with
 
-from bugbounty.env.database import db
 from .models import User
-from .serializer import register_user, user_response
+from .serializer import register_hacker, hacker_response, register_vendor, \
+    vendor_response
 
 bp = Blueprint('user', __name__)
 
 
-@bp.route('/api/users', methods=['POST'])
-@use_kwargs(register_user, locations=['json'])
-@marshal_with(user_response)
+@bp.route('/api/user', methods=('POST',))
+@use_kwargs(register_hacker, locations=['json'])
+@marshal_with(hacker_response)
 def register_user(**kwargs):
-    print(kwargs)
     try:
-        user = User(**kwargs).save()
+        user = User(is_vendor=False, **kwargs).save()
         return user
     except Exception:
-        db.session.rollback()
-        raise KeyError('error occurred')
+        raise Exception('Fail to register user')
+
+
+@bp.route('/api/user/<username>', methods=('GET',))
+@marshal_with(hacker_response)
+def get_user(username):
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return user
+    else:
+        raise Exception('User not found')
+
+
+@bp.route('/api/vendor', methods=('POST',))
+@use_kwargs(register_vendor, locations=['json'])
+@marshal_with(vendor_response)
+def register_vendor(**kwargs):
+    try:
+        vendor = User(is_vendor=True, **kwargs).save()
+        return vendor
+    except Exception:
+        raise Exception('Fail to register user')
